9d1df55f167328a98a1bd78dbc51a4ae2bebc517
FrancoMoreno144 <francokings.733@gmail.com>
2024-11-11 21:58:17 -0300

    Add files via upload

---
+++ crudbbdd.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-11-2024 a las 00:43:44
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `crudbbdd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `crudusers`
+--
+
+CREATE TABLE `crudusers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `crudusers`
+--
+
+INSERT INTO `crudusers` (`id`, `name`, `lastname`, `username`, `password`, `email`) VALUES
+(1, 'Franco', 'Moreno', 'Flan144', 'Hola123', 'Francokings.733@gmai');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `crudusers`
+--
+ALTER TABLE `crudusers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `crudusers`
+--
+ALTER TABLE `crudusers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
